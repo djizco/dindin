583884b6065ad8190c3911283b0f98b4fd59697b
Federico Rojas <rico@Federicos-MacBook-Pro.local>
2016-10-06 11:50:00 -0500

    Use layout and partials for views

--- views.js
+++ views.js
@@ -4,5 +4,8 @@ module.exports = {
   engines: { hbs: handlebars },
   relativeTo: __dirname,
   path: './views',
+  layoutPath: './views/layout',
+  layout: true,
   isCached: false,
+  partialsPath: './views/partials',
 }
